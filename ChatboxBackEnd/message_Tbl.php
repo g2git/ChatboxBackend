6878d4b8c6e0ad930d90491a26afef355902601e
g2git <35140511+g2git@users.noreply.github.com>
2018-01-26 17:41:50 +0100

    Add files via upload

---
+++ ChatboxBackEnd/message_Tbl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gimmy
+ * Date: 26-1-18
+ * Time: 17:21
+ */
+
+
+
+
+CREATE TABLE Message (
+-> id int(10) NOT NULL,
+->  username varchar(255) NOT NULL,
+->  mykey varchar(255) NOT NULL,
+->  value varchar(255) NOT NULL
+);
+
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+?>
